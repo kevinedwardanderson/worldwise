5535e2131a0227b09c1d5159743f63005281dcf6
Kevin Anderson <kevinanderson@Kevins-MacBook-Pro.local>
2024-02-21 23:46:14 -0500

    Minor update | Clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,12 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-// import { createContext, useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 
-// const BASE_URL = `http://localhost:8000`;
-
 function App() {
-  // const [cities, setCities] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // const CitiesContext = createContext();
-
-  // useEffect(function () {
-  //   async function fetchCities() {
-  //     try {
-  //       setIsLoading(true);
-  //       const res = await fetch(`${BASE_URL}/cities`);
-  //       const data = await res.json();
-
-  //       setCities(data);
-  //     } catch {
-  //       alert("There was an error loading data...");
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  //   fetchCities();
-  // }, []);
-
   return (
     <CitiesProvider>
       <BrowserRouter>
