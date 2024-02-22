d4544b60897fcbd6bc02e2a01ff01bfb693cbe28
Kevin Anderson <kevinanderson@Kevins-MacBook-Pro.local>
2024-02-21 23:29:44 -0500

    Updated state management to leverage Context API

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,15 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import {
+  CitiesContext,
+  CitiesProvider,
+  useCities,
+} from "../contexts/CitiesContext";
+import { useContext } from "react";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length) return <Message message={"No cities yet!"} />;
